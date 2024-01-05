d8a720bae9303d844ee43757e55cec3fb14913a0
cynthiachinenye <cynthiachiagu@gmail.com>
2024-01-05 15:34:04 +0100

    new

---
+++ src/Components/data.js
@@ -0,0 +1,34 @@
+
+const items =[
+    {
+        id:1,
+        title:"BBc News",
+        url:"https://www.bbc.com/news",
+        description:"latest new from the BBc",
+    },
+    {
+        id:2,
+        title:"TechCrunch News",
+        url:"https://www.techcrunch.com",
+        description:"latest new from the TechCrunch",
+    },
+    {
+        id:3,
+        title:" The Verge ",
+        url:"https://www.theverge.com",
+        description:"latest new from the Verge",
+    },
+    {
+        id:3,
+        title:" CNN News ",
+        url:"https://www.cnn.com/",
+        description:"latest new from the CNN",
+    },
+    {
+        id:3,
+        title:" Google News ",
+        url:"https://news.google.com",
+        description:"latest new from Google",
+    },
+]
+ export default items
